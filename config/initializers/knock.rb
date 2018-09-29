1cf7b47fc9fa3ea09dfd74a0f32fe6b01adff119
eckmLJE <lucas.eckman@gmail.com>
2018-09-29 15:17:12 -0400

    heroku

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -41,6 +41,7 @@ Knock.setup do |config|
   config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
   
 
+  
   ## If using Auth0, uncomment the line below
   # config.token_secret_signature_key = -> { JWT.base64url_decode Rails.application.secrets.auth0_client_secret }
 
