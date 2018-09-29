9f3e180014a3ffe25176d7b384fe934084372f4a
eckmLJE <lucas.eckman@gmail.com>
2018-09-29 16:46:31 -0400

    secret key base

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -38,7 +38,8 @@ Knock.setup do |config|
   ## Default:
   # config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
   # config.token_secret_signature_key = -> { Rails.application.credentials.read }
-  config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
+  # config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
+  config.token_secret_signature_key = -> { Rails.application.credentials.read }
   
 
   
