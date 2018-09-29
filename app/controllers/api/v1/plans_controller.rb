3c7f92dc36bd1fe998c46eb28999b549c3cdfa2d
eckmLJE <lucas.eckman@gmail.com>
2018-09-29 16:18:11 -0400

    gemfile changes

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PlansController < ApplicationController
 
-    # before_action :authenticate_user
+    before_action :authenticate_user
 
     def index
         render json: Plan.all
