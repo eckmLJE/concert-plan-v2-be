9d278f3f905e704bad03a8eb68cce8a735220f77
eckmLJE <lucas.eckman@gmail.com>
2018-10-16 16:09:17 -0400

    updated comments migration

--- db/migrate/20181002181903_create_comments.rb
+++ db/migrate/20181002181903_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
       t.integer :user_id
       t.integer :plan_id
+      t.string :user_name
       t.text :content
 
       t.timestamps
